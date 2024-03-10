45d085503df80b48e3cd6abaecc8fd81716400fc
Vu Van Dung <me@joulev.dev>
2024-03-10 16:42:47 +0800

    allow CORS
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- api/[station].tsx
+++ api/[station].tsx
@@ -8,7 +8,19 @@ function getStationNameFromRequest(req: Request) {
 export async function GET(req: Request) {
   const svg = await generateSvg(getStationNameFromRequest(req));
   return new Response(svg, {
-    headers: { "Content-Type": "image/svg+xml", "Cache-Control": "public, s-maxage=3600" },
+    headers: {
+      "Content-Type": "image/svg+xml",
+      "Cache-Control": "public, s-maxage=3600",
+    },
+  });
+}
+
+export function OPTIONS() {
+  return new Response(null, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+    },
   });
 }
 
